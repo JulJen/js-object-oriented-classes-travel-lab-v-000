2be8f2eb64ecac1be979e711c77e4e4ddc4345da
Jul Jen <juliajen.prog@gmail.com>
2018-09-06 00:53:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ class Route {
     return eastWest.indexOf(integer);
   }
 
-  blocksTravelled(horizontal, vertical) {
+  blocksTravelled(blocks) {
     const horzDist = (this.byAvenue(this.end.horizontal)) - (this.byAvenue(this.begin.horizontal));
 
     const vertDist = (this.end.vertical) - (this.begin.vertical);
@@ -38,11 +38,9 @@ class Route {
 
   // estimatedTime — The method returns the number of minutes estimated for the trip. The estimated time depends on the distance in blocks and whether the trip is occurring during peak hours or off peak hours.
   // During off peak hours, a driver travels three blocks in a minute, while during peak hours a driver travels an estimated two blocks in a minute.
-  
+
   estimatedTime(minutes) {
-    
+    const offPeak = blocksTravelled(3)
+    const Peak =
   }
 }
-
-
-
